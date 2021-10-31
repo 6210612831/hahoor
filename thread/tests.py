327d92323de5c864c486681c1666cf7d05f404fd
Pattana Sapphanachai <6210612831@cn330>
2021-10-31 14:32:05 +0700

    Test_File_01

--- thread/tests.py
+++ thread/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
 
+from thread.models import Sub_thread, Thread
+import user
+import datetime
+from django.utils import timezone
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+
+        en_password = make_password('1234')
+        user = User.objects.create(
+            username='user1', password=en_password, email='user1@exp.com')
+        thread = Thread.objects.create(header="Santa", content="#MARKDOWN_THREAD", author=user,
+                                       date=datetime.datetime.now(tz=timezone.utc), )
+        sub_thread = Sub_thread.objects.create(
+            replyto=thread, content="#MARKDOWN_REPLY", author=user, date=datetime.datetime.now(tz=timezone.utc))
+        thread.reply.add(sub_thread)
+        thread.save()
+# index view
+
+    def test_index_view_without_search(self):
+        c = Client()
+        responses = c.get(reverse('thread:index'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_index_view_with_search(self):
+        c = Client()
+        responses = c.post(reverse('thread:index'), {'search': 'Santa'})
+        self.assertEqual(responses.status_code, 200)
+
+    def test_index_view_with_search_but_cant_find(self):
+        c = Client()
+        responses = c.post(reverse('thread:index'), {'search': 'Twintown'})
+        self.assertEqual(responses.status_code, 200)
+
+# thread view
+
+    def test_thread_view(self):
+        c = Client()
+        responses = c.get(reverse('thread:thread', args=(1,)))
+        self.assertEqual(responses.status_code, 200)
+
+
+# my_thread view
+
+
+    def test_my_thread_view_without_authen(self):
+        c = Client()
+        responses = c.get(reverse('thread:my_thread'))
+        self.assertEqual(responses.status_code, 302)
+
+    def test_my_thread_view_with_authen(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.get(reverse('thread:my_thread'))
+        self.assertEqual(responses.status_code, 200)
+
+# create_thread view
+
+    def test_create_thread_view_without_authen(self):
+        c = Client()
+        responses = c.get(reverse('thread:create_thread'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_create_thread_view_with_post_and_already_have_this_header(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.post(reverse('thread:create_thread'), {'header': 'Santa',
+                                                             'Content': '## MARKDOWN CONTENT'})
+        self.assertEqual(responses.status_code, 200)
+
+    def test_create_thread_view_with_post(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.post(reverse('thread:create_thread'), {'header': 'HELLO WORLD',
+                                                             'Content': '## MARKDOWN CONTENT'})
+        self.assertEqual(responses.status_code, 302)
+
+    def test_create_thread_view_without_post(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.get(reverse('thread:create_thread'))
+        self.assertEqual(responses.status_code, 200)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,189 @@
-from django.test import TestCase
+from django.contrib.auth.hashers import make_password
+from django.urls import reverse
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+
+        en_password = make_password('1234')
+        User.objects.create(
+            username='user1', password=en_password, email='user1@exp.com')
+        User.objects.create(
+            username='user2', password=en_password, email='user2@exp.com', is_superuser='False')
+        User.objects.create(
+            username='admin', password=en_password, email='admin@exp.com', is_superuser='True')
+
+    def test_index_view(self):
+        c = Client()
+        responses = c.get(reverse('user:index'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_about_view(self):
+        c = Client()
+        responses = c.get(reverse('user:about'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_login_view(self):
+        c = Client()
+        responses = c.get(reverse('user:login'))
+        self.assertEqual(responses.status_code, 200)
+
+
+# login_check view
+
+
+    def test_login_check_view_with_authen(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.get(reverse('user:login_check'))
+        self.assertEqual(responses.status_code, 302)
+
+    def test_login_check_view_without_authen_and_no_post(self):
+        c = Client()
+        responses = c.get(reverse('user:login_check'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_login_check_view_without_authen_and_can_authen(self):
+        c = Client()
+        responses = c.post(reverse('user:login_check'),
+                           {'username': 'user1',
+                            'password': '1234'
+                            })
+        self.assertEqual(responses.status_code, 302)
+
+    def test_login_check_view_without_authen_and_can_not_authen(self):
+        c = Client()
+        responses = c.post(reverse('user:login_check'),
+                           {'username': 'randomuser',
+                            'password': '263514'
+                            })
+        self.assertEqual(responses.status_code, 302)
+
+
+# confirm_register view
+
+
+    def test_register_view_no_post(self):
+        c = Client()
+        responses = c.get(reverse('user:confirm_register'))
+        self.assertEqual(responses.status_code, 200)
+
+    def test_register_view_already_have_this_username(self):
+        c = Client()
+        responses = c.post(reverse('user:confirm_register'),
+                           {'username': 'user1',
+                            'password': '263514',
+                            're_password': '263514',
+                            'email': 'test@gmail.com',
+                            'first_name': 'Hello',
+                            'last_name': 'World',
+                            })
+        self.assertEqual(responses.status_code, 200)
+
+    def test_cregister_view_invalid_password(self):
+        c = Client()
+        # Password must have at least 8
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '2',
+                're_password': '2',
+                'email': 'test@gmail.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # Password must have at least 1 of a-z
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '12345678',
+                're_password': '12345678',
+                'email': 'test@gmail.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # Password must have at least 1 of A-Z
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '12345678a',
+                're_password': '12345678a',
+                'email': 'test@gmail.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # Password must have at least 1 of 0-9
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testtest',
+                'password': 'dwadadsawsaASDSADW',
+                're_password': 'dwadadsawsaASDSADW',
+                'email': 'test@gmail.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # Invalid password confirm
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '12345asdWQDSA',
+                're_password': '123456321',
+                'email': 'test@gmail.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # This email is already taken
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '12345asdWQDSA',
+                're_password': '12345asdWQDSA',
+                'email': 'user1@exp.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+        # Can register
+        c.post(reverse('user:confirm_register'),
+               {'username': 'testuser',
+                'password': '12345asdWQDSA',
+                're_password': '12345asdWQDSA',
+                'email': 'test@exp.com',
+                'first_name': 'Hello',
+                'last_name': 'World',
+                })
+
+# logout view
+    def test_logout_view(self):
+        c = Client()
+        responses = c.get(reverse('user:logout'))
+        self.assertEqual(responses.status_code, 302)
+
+
+# register_view view
+
+
+    def test_register_view_view(self):
+        c = Client()
+        responses = c.get(reverse('user:register'))
+        self.assertEqual(responses.status_code, 200)
+
+# admin_view view
+
+    def test_admin_view_view_without_authen(self):
+        c = Client()
+        responses = c.get(reverse('user:admin'))
+        self.assertEqual(responses.status_code, 302)
+
+    def test_admin_view_view_without_superuser(self):
+        c = Client()
+        user = User.objects.get(username='user1', email='user1@exp.com')
+        c.force_login(user)
+        responses = c.get(reverse('user:admin'))
+        self.assertEqual(responses.status_code, 302)
+
+    def test_admin_view_view_with_superuser(self):
+        c = Client()
+        user = User.objects.get(username='admin', email='admin@exp.com')
+        c.force_login(user)
+        responses = c.get(reverse('user:admin'))
+        self.assertEqual(responses.status_code, 200)
